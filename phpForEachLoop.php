7c7f0557caa9d03b9cca975947622fb8f966774b
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2021-10-20 09:40:04 -0300

    chore: implemented foreach example

---
+++ phpForEachLoop.php
@@ -0,0 +1,23 @@
+<?php 
+
+	$meses = array(
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  );
+
+	foreach ($months as $index => $month) {
+		echo "The index is: ".$index."<br>";
+		echo "The month is: ".$month."<br>";
+	}
+
+ ?>
